e6281ffafebea8ba655a611d98acde2df12b0897
Oleg <olegbur_2000@mail.ru>
2025-02-22 06:11:30 +0300

    netfly cfg

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // Удалено incrementalRegeneration, так как эта опция больше не поддерживается
-  },
   images: {
-    formats: ['image/avif', 'image/webp'],
-    domains: ['localhost'], // Добавьте необходимые домены
+    formats: ["image/avif", "image/webp"],
+    domains: ["localhost"], // Добавь нужные домены
   },
   async headers() {
     return [
       {
-        source: '/:path*',
+        source: "/(.*)", // Применяется ко всем маршрутам
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'"
-          }
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; frame-src 'self' https://yandex.ru",
+          },
         ],
       },
-    ]
+    ];
   },
 };
 
