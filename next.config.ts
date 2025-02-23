f3f2c3a52eefa7406572067882d573fb1ab8c511
Oleg <olegbur_2000@mail.ru>
2025-02-23 11:25:55 +0300

    test3

--- next.config.ts
+++ next.config.ts
@@ -18,11 +18,11 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value: [
-              // Базовые настройки
+              // Основные директивы
               "default-src 'self'",
               
               // Скрипты
-              "script-src 'self' 'unsafe-eval' https://jsisjfkucnluxaaxraxt.supabase.co",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://jsisjfkucnluxaaxraxt.supabase.co",
               
               // Стили
               "style-src 'self' 'unsafe-inline'",
@@ -31,13 +31,17 @@ const nextConfig: NextConfig = {
               "img-src 'self' data: https://*.supabase.co",
               
               // Шрифты
-              "font-src 'self' data:",
+              "font-src 'self' data: https:",
               
               // Подключения
               "connect-src 'self' https://*.supabase.co wss://*.supabase.co",
               
               // Фреймы
-              "frame-src 'self'"
+              "frame-src 'self' https://yandex.ru",
+              
+              // Дополнительные настройки
+              "base-uri 'self'",
+              "form-action 'self'"
             ].join("; "),
           },
         ],
