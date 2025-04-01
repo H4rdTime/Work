34df2b4849f0c898a1e42c8ef7a827eb0f2a5b7f
Oleg <olegbur_2000@mail.ru>
2025-04-01 23:38:35 +0300

    new map

--- src/app/components/MapIframe.tsx
+++ src/app/components/MapIframe.tsx
@@ -4,7 +4,7 @@ const MapIframe = () => {
   return (
     <div className="relative w-full aspect-video rounded-xl overflow-hidden shadow-xl">
       <iframe
-        src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad9c2ce5f3f93b1220e44539e76f2fae4b500f45a0280f9c51d63abbfd1dfa625&amp;source=constructor"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A935501884654b56b8adabbe548e6fbefbcb498b5067ecb1ba8aeb2bd6724ea4e&amp;source=constructor"
         className="absolute top-0 left-0 w-full h-full"
         title="Карта"
         loading="lazy"
