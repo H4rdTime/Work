f1a5421c171ac6ff870107cb49c791e964fa7435
Oleg <olegbur_2000@mail.ru>
2025-05-22 20:47:24 +0300

    mc fix 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { ReactNode } from 'react';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Script from 'next/script';
-import Image from 'next/image';
+// import Image from 'next/image'; // <-- Этот импорт удален, так как мы больше не используем next/image для метрики
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -46,40 +46,41 @@ export default function RootLayout({ children }: LayoutProps) {
       <body className="bg-white">
         {children}
 
-        {/* Yandex.Metrika */}
+        {/* Yandex.Metrika counter */}
+        {/* Основной скрипт загружается с помощью next/script - это правильно */}
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for(var j=0;j<document.scripts.length;j++){
-                if(document.scripts[j].src===r) return;
-              }
-              k=e.createElement(t);
-              a=e.getElementsByTagName(t)[0];
-              k.async=1; k.src=r;
-              a.parentNode.insertBefore(k,a);
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(101237027, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
+            ym(101237027, "init", { // Проверьте ваш номер счетчика
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
             });
           `}
         </Script>
+        {/* <noscript> часть с обычным HTML тегом <img> */}
         <noscript>
           <div>
-            <Image
-              src="https://mc.yandex.ru/watch/101237027"
+            {/* Здесь используем стандартный <img> тег, а не next/image */}
+            <img
+              src="https://mc.yandex.ru/watch/101237027" // Проверьте ваш номер счетчика
               style={{ position: 'absolute', left: '-9999px' }}
               alt=""
             />
           </div>
         </noscript>
+        {/* /Yandex.Metrika counter */}
 
+        {/* Google Analytics counter */}
+        {/* Загружается с помощью @next/third-parties/google - это правильно */}
         <GoogleAnalytics gaId="G-0YHV1T38RB" />
+        {/* /Google Analytics counter */}
       </body>
     </html>
   );
